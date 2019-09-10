7ee60c34eb534376c75aa3757586dcfc1f17e0b9
Clemerson90 <32943537+Clemerson90@users.noreply.github.com>
2019-09-09 21:47:06 -0300

    Calculo do resto de uma divisão
    Calcula o resto de uma divisão

---
+++ Calculadora.java
@@ -0,0 +1,31 @@
+package br.com.faec;
+/**
+ * 
+ * @author Clemerson Santos
+ * 
+ */
+
+public class Calculadora {
+	int num1;
+	int num2;
+	Double resto = 0.0;
+	Double calc= 0.0;
+	
+	Double obterResto(int num1, int num2) {
+		
+		resto = (double) (num1 % num2);
+		
+		return resto;
+	}
+
+	public static void main(String args[]) {
+		Calculadora calc = new Calculadora();
+		calc.num1 =10;
+		calc.num2 =7;
+		
+		Double resto =calc.obterResto(calc.num1, calc.num2);
+		System.out.println(resto);
+			
+	}
+
+}
